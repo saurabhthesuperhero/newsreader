b6b04a30fe8a1030de367ada932a5300cf3374b9
Saurabh Jadhav <40573988+saurabhthesuperhero@users.noreply.github.com>
2019-05-07 23:30:01 +0530

    completed app love u
    wow

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,21 @@ import 'dart:ui' as ui;
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 void main()=>runApp(new MaterialApp(
-  home: HomePage(),
+  home: category(),
 ));
 
 class HomePage extends StatefulWidget {
+  HomePage(this.url);
+  final String url;
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState(url);
 }
 
 class _HomePageState extends State<HomePage> {
 
-  final String url="https://newsapi.org/v2/top-headlines?country=in&apiKey=a623caffeac64ef480c975e30bcca20b";
+  _HomePageState(this.url);
+  //final String url="https://newsapi.org/v2/top-headlines?country=in&apiKey=a623caffeac64ef480c975e30bcca20b";
+  final String url;
   List data;
  
   @override
@@ -104,6 +108,322 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+class category extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(67, 67, 67,1),
+        title: new Text("Perfect News", style: new TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      body: new SafeArea(
+          child: new Column(
+        children: [
+          new Expanded(
+            flex: 1,
+            child: new Container(
+              width: width,
+              color: Colors.white,
+              child: new Center(
+                  child: new GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                children: <Widget>[
+                  new Card(
+                      elevation: 5.0,
+                      child: new GestureDetector(
+                        child: new Container(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/bsns.png",
+                                alignment: Alignment.center,
+                                width: 70.0,
+                                height: 70.0,
+                              ),
+                              new Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Text(
+                                  "Business Headlines",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          var id = 1;
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new HomePage("https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                              ));
+                        },
+                      )),
+                  new Card(
+                      elevation: 5.0,
+                      child: new GestureDetector(
+                        child: new Container(
+                          child: new Column(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/drama.png",
+                                alignment: Alignment.center,
+                                width: 70.0,
+                                height: 70.0,
+                              ),
+                              new Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Text("Entertainment",
+                                    style: TextStyle(
+                                        fontSize: 20.0, color: Colors.black),
+                                    textAlign: TextAlign.center),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          var id = 2;
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new HomePage("https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                              ));
+                        },
+                      )),
+                  new Card(
+                      elevation: 5.0,
+                      child: new GestureDetector(
+                        child: new Container(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/health.png",
+                                alignment: Alignment.center,
+                                width: 70.0,
+                                height: 70.0,
+                              ),
+                              new Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Text("Health",
+                                    style: TextStyle(
+                                        fontSize: 20.0, color: Colors.black),
+                                    textAlign: TextAlign.center),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          var id = 3;
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new HomePage("https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                              ));
+                        },
+                      )),
+                  new Card(
+                    elevation: 5.0,
+                    child: new GestureDetector(
+                      child: new Container(
+                        child: Column(
+                          children: <Widget>[
+                            new Padding(
+                              padding: EdgeInsets.all(10.0),
+                            ),
+                            Image.asset(
+                              "assets/science.png",
+                              alignment: Alignment.center,
+                              width: 80.0,
+                              height: 80.0,
+                            ),
+                            new Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text("Science",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.black),
+                                  textAlign: TextAlign.center),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        var id = 4;
+                        Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  new HomePage("https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                            ));
+                      },
+                    ),
+                  ),
+                  new Card(
+                      elevation: 5.0,
+                      child: new GestureDetector(
+                        child: new Container(
+                          child: new Column(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/sports.png",
+                                alignment: Alignment.center,
+                                width: 70.0,
+                                height: 70.0,
+                              ),
+                              new Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Text("Sports",
+                                    style: TextStyle(
+                                        fontSize: 20.0, color: Colors.black),
+                                    textAlign: TextAlign.center),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          var id = 5;
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new HomePage("https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                              ));
+                        },
+                      )),
+
+              new Card(
+                      elevation: 5.0,
+                      child: new GestureDetector(
+                        child: new Container(
+                          child: new Column(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/tech.png",
+                                alignment: Alignment.center,
+                                width: 70.0,
+                                height: 70.0,
+                              ),
+                              new Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Text("Technology",
+                                    style: TextStyle(
+                                        fontSize: 20.0, color: Colors.black),
+                                    textAlign: TextAlign.center),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          var id = 5;
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new HomePage("https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                              ));
+                        },
+                      )),
+
+
+                      //bro adding more cards....ok
+                       new Card(
+                      elevation: 5.0,
+                      child: new GestureDetector(
+                        child: new Container(
+                          child: new Column(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/ggl.png",
+                                alignment: Alignment.center,
+                                width: 70.0,
+                                height: 70.0,
+                              ),
+                              new Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Text("Google",
+                                    style: TextStyle(
+                                        fontSize: 20.0, color: Colors.black),
+                                    textAlign: TextAlign.center),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          var id = 5;
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new HomePage("https://newsapi.org/v2/everything?q=google&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                              ));
+                        },
+                      )),
+
+                      //bro adding more cards....ok
+                       new Card(
+                      elevation: 5.0,
+                      child: new GestureDetector(
+                        child: new Container(
+                          child: new Column(
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/pl.png",
+                                alignment: Alignment.center,
+                                width: 70.0,
+                                height: 70.0,
+                              ),
+                              new Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Text("Programming",
+                                    style: TextStyle(
+                                        fontSize: 20.0, color: Colors.black),
+                                    textAlign: TextAlign.center),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          var id = 5;
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    new HomePage("https://newsapi.org/v2/everything?q=programming&apiKey=a623caffeac64ef480c975e30bcca20b"),
+                              ));
+                        },
+                      )),
+
+                      
+
+
+                ],
+              )),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
 
 class Mysimpleview extends StatelessWidget {
   Mysimpleview(this.title,this.image,this.data,this.link);
